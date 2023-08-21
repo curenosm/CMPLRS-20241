f40a6b3fb12a0a6bfa65f4662de34b6d5b2876d8
Misael Cureño <curenosm@gmail.com>
2023-08-21 11:56:49 -0600

    Update tests.hs

--- practicas/practica_1/tests.hs
+++ practicas/practica_1/tests.hs
@@ -1,18 +1,14 @@
 import qualified Data.Set as Set
 import Data.List (nub)
 
+charsSet :: String -> Set.Set Char
+charsSet word = Set.fromList word
 
 isAnagram :: String -> String -> Bool
 isAnagram s1 s2 = length s1 == length s2 && charsSet s1 == charsSet s2
-    where 
-        charsSet :: String -> Set.Set Char
-        charsSet word = Set.fromList word
 
 getAnagrams :: String -> [String] -> [String]
 getAnagrams s l = filter (\y -> isAnagram y s) l
 
 groupAnagrams :: [String] -> [[String]]
 groupAnagrams l = nub (map (\e -> getAnagrams e l) l)
-
-groupAnagrams ["hello","", "world", "wldro", "hlloe", "a", "aa"]
-groupAnagrams ["eat" ,"tea", "tan", "ate", "nat", "bat"]
