2e9a816acb75309cbbc90c079597006aa8c5c223
Raúl Bernal <raulbernal@ciencias.unam.mx>
2023-08-27 20:06:17 -0600

    Agregando solución a practica1/Practica1.hs

--- practica1/Practica1.hs
+++ practica1/Practica1.hs
@@ -4,3 +4,11 @@ fibonacci :: Int -> Int
 fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci n = fibonacci (n-1) + fibonacci (n-2)
+
+-- Ejercicio 4
+coins :: [Int] -> Int -> Bool
+coins [] n = n == 0
+coins (x:xs) n
+  | n > 0 = coins (x:xs) (n - x) || coins xs (n - x) || coins xs n
+  | n < 0 = False
+  | n == 0 = True
