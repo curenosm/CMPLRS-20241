c27ead8848fc3260dbdaef6c2213720fc64824a0
Misael Cureño <curenosm@gmail.com>
2023-09-22 16:45:18 -0600

    Incluyendo ejemplo Alex visto en ayudantía para basarnos en él

--- practica3/test.hs
+++ practica3/test.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Tokens (lexer)
 import Practica3
   ( Token(Assign, If, Then, Else, Seq, While, Do, Skip, Boolean, Equal, And, Not, Loc, Number, LP, RP, Sum)
   )
@@ -8,10 +9,16 @@ import Control.Exception (evaluate, SomeException, try)
 test_1 = TestCase (assertEqual "1" 1 1)
 test_2 = TestCase (assertEqual "2" 2 2)
 
+test_alex_lexer_1 = TestCase (assertEqual "lexer \"x := 1\""
+  [Assign, Loc "x", Equal, Number 1]
+  (lexer "x := 1"))
+
+
 tests =
   TestList
     [ test_1
     , test_2
+    , test_alex_lexer_1
     ]
 
 main = runTestTT tests
