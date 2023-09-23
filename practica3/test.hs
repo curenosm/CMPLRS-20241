fba399e00a16d2313148b86e02df7f18a6ef443e
Raúl Bernal <raulbernal@ciencias.unam.mx>
2023-09-22 21:07:13 -0600

    Agregando tests del pdf de la práctica

--- practica3/test.hs
+++ practica3/test.hs
@@ -1,9 +1,5 @@
 import Data.List
-import Tokens(alexScanTokens, Token (
-  Assign, If, Then, Else,
-  Seq, While, Do, Skip,
-  Boolean, Equal, And, 
-  Not, Loc, Number, LP, RP, Sum))
+import Main
 import Test.HUnit
 import Control.Exception (evaluate, SomeException, try)
 
@@ -14,12 +10,21 @@ test_alex_lexer_1 = TestCase (assertEqual "alexScanTokens \"L000000001 := 1\""
   [Loc 1, Assign, Number 1]
   (alexScanTokens "L1 := 1"))
 
+test_alex_lexer_2 = TestCase (assertEqual "alexScanTokens \"L2:=1; L3:=0; while -(L2=L2) do L2:=L2+1; L3:=L3+1\""
+  [Loc 2, Assign, Number 1, Seq, Loc 3, Assign, Number 0, Seq, While, Not, LP, Loc 2, Equal, Loc 2, RP, Do, Loc 2, Assign, Loc 2, Sum, Number 1, Seq, Loc 3, Assign, Loc 3, Sum, Number 1]
+  (alexScanTokens "L2:=1; L3:=0; while -(L2=L2) do L2:=L2+1; L3:=L3+1"))
+
+test_alex_lexer_3 = TestCase (assertEqual "alexScanTokens \"if -(true&false) then skip else skip\""
+  [If, Not, LP, Boolean True, And, Boolean False, RP, Then, Skip, Else, Skip]
+  (alexScanTokens "if -(true&false) then skip else skip"))
 
 tests =
   TestList
     [ test_1
     , test_2
     , test_alex_lexer_1
+    , test_alex_lexer_2
+    , test_alex_lexer_3
     ]
 
 main = runTestTT tests
