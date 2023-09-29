87de1351d848882d9d8a7fc3a23abb162d8c9757
Uriel García Luna Bobadilla <leiru06@MacBook-Pro-de-Uriel-2.local>
2023-09-28 19:22:29 -0600

    :white_check_mark: Agregando más pruebas al lexer, incluso las que se muestran en el pdf

--- practica3/test.hs
+++ practica3/test.hs
@@ -38,6 +38,10 @@ test_lexer_2 = TestCase (assertEqual "lexer \" \" " [] (lexer " "))
 test_lexer_3 = TestCase (assertEqual "lexer \"\n\"" [] (lexer "\n"))
 test_lexer_4 = TestCase (assertEqual "lexer \"\t\"" [] (lexer "\t"))
 test_lexer_5 = TestCase (assertEqual "lexer \";\"" [Seq] (lexer ";"))
+test_lexer_6 = TestCase (assertEqual "lexer \"L2:=1;\""[ Loc 2 , Assign, Number 1, Seq ] (lexer "L2 :=1;"))
+test_lexer_7 = TestCase (assertEqual "lexer \"L2:=1; L3:=0;\""[ Loc 2 , Assign, Number 1, Seq, Loc 3, Assign, Number 0, Seq ] (lexer "L2:=1; L3:=0;"))
+test_lexer_8 = TestCase (assertEqual "lexer \"L2:=1; L3:=0; while -(L2=L2) do L2:=L2+1; L3:=L3+1\""[ Loc 2 , Assign, Number 1, Seq, Loc 3, Assign, Number 0, Seq, While, Not, LP, Loc 2, Equal, Loc 2, RP, Do, Loc 2, Assign, Loc 2, Sum, Number 1, Seq, Loc 3, Assign, Loc 3, Sum, Number 1] (lexer "L2:=1; L3:=0; while -(L2=L2) do L2:=L2+1; L3:=L3+1"))
+test_lexer_9 = TestCase (assertEqual "lexer \"if -(true&false) then skip else skip\""[If, Not, LP, Boolean True, And, Boolean False, RP, Then, Skip, Else, Skip] (lexer "if -(true&false) then skip else skip"))
 
-tests = TestList [ test_lexer_1 ]
+tests = TestList [ test_lexer_1, test_lexer_2, test_lexer_3, test_lexer_4, test_lexer_5, test_lexer_6, test_lexer_7, test_lexer_8, test_lexer_9 ]
 main = runTestTT tests
