ce598ae54368c608cfe54e3b874369a5fe337811
Raúl Bernal <raulbernal@ciencias.unam.mx>
2023-10-13 13:33:27 -0600

    Dejando solamente funciones y definiciones necesarias

--- practica4/Practica4.hs
+++ practica4/Practica4.hs
@@ -6,40 +6,10 @@
 
 module Practica4 where
 
-import Data.Char
-import Data.List (nub)
-
-
 data Token = Assign | If | Then | Else | Seq | While | Do | Skip | Boolean Bool | Equal | And | Not | Loc Int | Number Int | LP | RP | Sum deriving (Eq, Show)
 
-lexer :: String -> [Token]
-lexer [] = []
-lexer (' ':xs) = lexer xs
-lexer ('\n':xs) = lexer xs
-lexer ('\t':xs) = lexer xs
-lexer (';':xs) = Seq : lexer xs
-lexer ('&':xs) = And : lexer xs
-lexer ('-':xs) = Not : lexer xs
-lexer ('=':xs) = Equal : lexer xs
-lexer (':':'=':xs) = Assign : lexer xs
-lexer ('s':'k':'i':'p':xs) = Skip : lexer xs
-lexer ('i':'f':xs) = If : lexer xs
-lexer ('t':'h':'e':'n':xs) = Then : lexer xs
-lexer ('e':'l':'s':'e':xs) = Else : lexer xs
-lexer ('w':'h':'i':'l':'e':xs) = While : lexer xs
-lexer ('d':'o':xs) = Do : lexer xs
-lexer ('(':xs) = LP : lexer xs
-lexer (')':xs) = RP : lexer xs
-lexer ('+':xs) = Sum : lexer xs
-lexer ('t':'r':'u':'e':xs) = Boolean True : lexer xs
-lexer ('f':'a':'l':'s':'e':xs) = Boolean False : lexer xs
-lexer (x:xs)
-    | isDigit x = Number (read (x : takeWhile isDigit xs)) : lexer (dropWhile isDigit xs)
-    | x == 'L' = Loc (read (takeWhile isDigit xs)) : lexer (dropWhile isDigit xs)
-    | otherwise = error ("lexer: unexpected character " ++ [x])
-
-
 data Content = T Token | S | C | B | E deriving (Eq, Show)
+
 type Input = [Token]
 type Stack = [Content]
 
@@ -89,7 +59,3 @@ parser input = parserAux input [S]
 
 {- Ejemplo -}
 --parser [LP, Loc 2, Assign, Number 1, Seq, LP, Loc 3, Assign, Number 0, Seq, While, Not, Loc 2, Equal, Loc 2, Do, LP, Loc 2, Assign, LP, Loc 2, Sum, Number 1, RP, Seq, Loc 3, Assign, LP, Loc 3, Sum, Number 1, RP, RP, RP, RP]
-
-data C = AssignASA E E | IfThenElse B C C | SeqASA C C | WhileDo B C | SkipASA deriving (Eq, Show)
-data B = BoolASA Bool | EqualASA E E | AndASA B B | NotASA B deriving (Eq, Show)
-data E = LocASA Int | NumberASA Int | SumASA E E deriving (Eq, Show)
