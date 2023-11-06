5b52714a2798c0f65d4a28f6078febf0fe6a2816
Uriel García Luna Bobadilla <leiru06@MacBook-Pro-de-Uriel-2.local>
2023-11-06 16:44:01 -0600

    Primera implementación

--- practica5/Practica5.hs
+++ practica5/Practica5.hs
@@ -21,10 +21,10 @@ data Token =  Assign
     | Number Int
     | LP
     | RP
-    | Sum deriving (Eq, Show)
+    | Sum deriving Show
 
-data Content = T Token | S | C | B | E deriving (Eq, Show)
-data State = Q Int deriving (Eq, Show)
+data Content = T Token | S | C | PC | B | PB | E | PE deriving Show
+data State = Q Int deriving Show
 
 type Input = [Token]
 type Stack = [State]
@@ -32,19 +32,37 @@ type Symbols = [Content]
 
 -- TODO: Implementar
 parserAux :: Input -> Stack -> Symbols -> Bool
+--Shift
+parserAux (Loc l : xs) (Q 0 : ys) ws = parserAux xs (Q 3 : Q 0 : ys) (T (Loc l) : ws)
+parserAux (Skip : xs) (Q 0 : ys) ws = parserAux xs (Q 4 : Q 0 : ys) (T Skip : ws)
+parserAux (Seq : xs) (Q 2 : ys) ws = parserAux xs (Q 5 : Q 2 : ys) (T Seq : ws)
+parserAux (Assign : xs) (Q 3 : ys) ws = parserAux xs (Q 6 : Q 3 : ys) (T Assign : ws)
+parserAux (Loc l : xs) (Q 5 : ys) ws = parserAux xs (Q 3 : Q 5 : ys) (T (Loc l) : ws)
+parserAux (Skip : xs) (Q 5 : ys) ws = parserAux xs (Q 4 : Q 5 : ys) (T Skip : ws)
+parserAux (Loc l : xs) (Q 6 : ys) ws = parserAux xs (Q 10 : Q 6 : ys) (T (Loc l) : ws)
+parserAux (Number n : xs) (Q 6 : ys) ws = parserAux xs (Q 11 : Q 6 : ys) (T (Number n) : ws)
+parserAux (Sum : xs) (Q 9 : ys) ws = parserAux xs (Q 12 : Q 9 : ys) (T Sum : ws)
+parserAux (Loc l : xs) (Q 12 : ys) ws = parserAux xs (Q 10 : Q 12 : ys) (T (Loc l) : ws)
+parserAux (Number n : xs) (Q 12 : ys) ws = parserAux xs (Q 11 : Q 12 : ys) (T (Number n) : ws)
+--Reduce
+parserAux [] (Q 4 : ys) (T Skip : ws) = parserAux [] (Q 2 : ys) (PC : ws)
+parserAux [] (Q 2 : ys) (PC : ws ) = parserAux [] (Q 1 : ys) (C : ws)
+--Accept
+parserAux [] (Q 1 : ys) ws = True
+parserAux _ _ _ = False
 
 {- Ejemplo -}
-parserAux [Loc 2
-    , Assign, Number 1, Seq, Loc 3, Assign, Number 0, Seq
-    , Number 0, Seq, While, Not, Loc 2, Equals, Loc 2, Do
-    , Loc 2, Assign , LP, Loc 2, Sum, Number 1, Seq, Loc 3
-    , Assign, LP, Loc 3, Sum, Number 1
-    ] [Q 0] [] -- False
+-- parserAux [Loc 2
+--     , Assign, Number 1, Seq, Loc 3, Assign, Number 0, Seq
+--     , Number 0, Seq, While, Not, Loc 2, Equals, Loc 2, Do
+--     , Loc 2, Assign , LP, Loc 2, Sum, Number 1, Seq, Loc 3
+--     , Assign, LP, Loc 3, Sum, Number 1
+--     ] [Q 0] [] -- False
 
-parserAux [Loc 1
-        , Assign, Number 1, Seq, Loc 2
-        , Assign, Number 2, Seq, Skip
-        ] [Q 0] [] -- True
+-- parserAux [Loc 1
+--         , Assign, Number 1, Seq, Loc 2
+--         , Assign, Number 2, Seq, Skip
+--         ] [Q 0] [] -- True
 
 -- TODO: Implementar
-parser :: Input -> Bool
+-- parser :: Input -> Bool
