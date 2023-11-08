4628485ee69f5f7c283ddf41d0b6bba89144d2e1
Uriel García Luna Bobadilla <leiru06@MacBook-Pro-de-Uriel-2.local>
2023-11-08 09:47:57 -0600

    Implementando parser

--- practica5/Practica5.hs
+++ practica5/Practica5.hs
@@ -4,7 +4,6 @@
 -- 316641902 García Luna Bobadilla Uriel
 -- 419002237 Jardón Cárdenas Juan Diego
 
-
 data Token =  Assign
     | If
     | Then
@@ -53,30 +52,32 @@ parserAux (Number n : xs) (Q 12 : ys) ws = parserAux xs (Q 11 : Q 12 : ys) ( T (
 --Verificamos que los simbolos y estados que vamos a descartar son correctos
 --Continuamos la derivacion reemplazando estados y simbolos
 parserAux (Seq : xs) (Q 4 : ys) (T Skip : ws) = parserAux (Seq : xs) (Q 2:ys) (PC:ws)
-parserAux (Seq : xs) (Q 8 : Q 6 : Q 3 : Q 0 : ys) (T (Loc l) : T Assign : E : ws) = parserAux (Seq : xs) (Q 2 : Q 0: ys) (PC : ws)
+parserAux (Seq : xs) (Q 8 : Q 6 : Q 3 : ys) (E : ws) = parserAux (Seq : xs) (Q 2 : Q 0: ys) (PC : ws)
 parserAux (Seq : xs) (Q 9 : Q 6 : ys) (PE : ws) = parserAux (Seq : xs) (Q 8 : Q 6 : ys) (E : ws)
 parserAux (Seq : xs) (Q 9 : Q 12 : ys) (PE : ws) = parserAux (Seq : xs) (Q 13 : Q 12 : ys) (E : ws)
 parserAux (Seq : xs) (Q 10 : Q 6 : ys) (T (Loc l) : ws) = parserAux (Seq : xs) (Q 9 : Q 6 : ys) (PE : ws)
 parserAux (Seq : xs) (Q 10 : Q 12 : ys) (T (Loc l) : ws) = parserAux (Seq : xs) (Q 9 : Q 12 : ys) (PE : ws)
 parserAux (Seq : xs) (Q 11 : Q 6 : ys) (T (Number n) : ws) = parserAux (Seq : xs) (Q 9 : Q 6 : ys) (PE : ws)
 parserAux (Seq : xs) (Q 11 : Q 12 : ys) (T (Number n) : ws) = parserAux (Seq : xs) (Q 9 : Q 12 : ys) (PE : ws)
-parserAux (Seq : xs) (Q 13 : Q 12 : ys) ( PE : T (Number n) : ws ) = parserAux (Seq : xs) (Q 9 : Q 12 : ys) (PE : ws)
+parserAux (Seq : xs) (Q 13 : Q 12 : ys) (E : ws ) = parserAux (Seq : xs) (Q 9 : Q 12 : ys) (PE : ws)
 parserAux (Sum : xs) (Q 10 : Q 6 : ys) (T (Loc l) : ws) = parserAux (Sum : xs) (Q 9 : Q 6 : ys) (PE : ws)
 parserAux (Sum : xs) (Q 10 : Q 12 : ys) (T (Loc l) : ws) = parserAux (Sum : xs) (Q 9 : Q 12 : ys) (PE : ws)
 parserAux (Sum : xs) (Q 11 : Q 6 : ys) (T (Number n) : ws) = parserAux (Sum : xs) (Q 9 : Q 6 : ys) (PE : ws)
 parserAux (Sum : xs) (Q 11 : Q 12 : ys) (T (Number n) : ws) = parserAux (Sum : xs) (Q 9 : Q 12 : ys) (PE : ws)
-parserAux [] (Q 4 : ys) (T Skip : ws) = parserAux [] (Q 2:ys) (PC:ws)
+parserAux [] (Q 4 : ys) (T Skip : ws) = parserAux [] (Q 2:ys) (PC : ws)
 parserAux [] (Q 2 : ys) (PC : ws) = parserAux [] (Q 1 : ys) (C : ws)
-
--- parserAux [] (Q 7 : Q 5 : Q 2 : ys) (C : T Seq : PC : ws) = parserAux [] (Q 1 : ys) (C : ws)
--- parserAux [] (Q 8 : Q 6 : Q 3 : ys) (E : T Assign : T (Loc l) : ws) = parserAux [] (Q 1 : ys) (C : ws)
--- parserAux [] (Q 9 : Q 6 : Q 3 : ys) (PE : T Assign : T (Loc l) : ws) = parserAux [] (Q 1 : ys) (C : ws) 
--- parserAux [] (Q 10 : Q 6 : Q 3 : ys) (T (Loc a) : T Assign : T (Loc b) : ws) = parserAux [] (Q 1 : ys) (C : ws)
--- parserAux [] (Q 11 : Q 6 : Q 3 : ys) (T (Number n) : T Assign : T (Loc l) : ws) = parserAux [] (Q 1 : ys) (C : ws)
--- parserAux [] (Q 13 : Q 12 : Q 9 : Q 6 : Q 3 : ys) (E : T Sum : PE : T Assign : T (Loc l) : ws) = parserAux [] (Q 1 : ys) (C : ws)
+parserAux [] (Q 7 : Q 5 : ys) (C : ws) = parserAux [] (Q 2 : ys) (PC : ws)
+parserAux [] (Q 8 : Q 6 : ys) (E : ws) = parserAux [] (Q 2: ys) (PC : ws)
+parserAux [] (Q 9 : Q 6 : ys) (PE : ws) = parserAux [] (Q 8 : Q 6 : ys) (E : ws)
+parserAux [] (Q 9 : Q 12 : ys) (PE : ws) = parserAux [] (Q 13 : Q 12 : ys) (E : ws)
+parserAux [] (Q 10 : Q 6 : ys) (T (Loc l) : ws) = parserAux [] (Q 9 : Q 6 : ys) (PE : ws)
+parserAux [] (Q 10 : Q 12 : ys) (T (Loc l) : ws) = parserAux [] (Q 9 : Q 12 : ys) (PE : ws)
+parserAux [] (Q 11 : Q 6 : ys) (T (Number n) : ws) = parserAux [] (Q 9 : Q 6 : ys) (PE : ws)
+parserAux [] (Q 11 : Q 12 : ys) (T (Number n) : ws) = parserAux [] (Q 9 : Q 12 : ys) (PE : ws)
+parserAux [] (Q 13 : Q 12 : ys) (E : ws ) = parserAux [] (Q 9 : Q 12 : ys) (PE : ws)
 --ACCEPT
 parserAux [] (Q 1:ys) ws = True
-parserAux _ _ _ = False
+parserAux _ _ _ = False 
 
 {- Ejemplo -}
 -- parserAux [Loc 2
@@ -86,10 +87,8 @@ parserAux _ _ _ = False
 --     , Assign, LP, Loc 3, Sum, Number 1
 --     ] [Q 0] [] -- False
 
--- parserAux [Loc 1
---         , Assign, Number 1, Seq, Loc 2
---         , Assign, Number 2, Seq, Skip
---         ] [Q 0] [] -- True
+-- parserAux [Loc 1, Assign, Number 1, Seq, Loc 2, Assign, Number 2, Seq, Skip] [Q 0] [] -- True
 
 -- TODO: Implementar
--- parser :: Input -> Bool
+parser :: Input -> Bool
+parser xs = parserAux xs [Q 0] []
